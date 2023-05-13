2de7b7fdf753bd9bd349a3b29a3c1d4d3e286741
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-13 14:17:10 -0500

    First pass at get_my_weather function.

--- umbrella_reminder.py
+++ umbrella_reminder.py
@@ -4,6 +4,7 @@
 
 import logging
 import os
+from requests_html import HTMLSession
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -11,3 +12,22 @@ logging.basicConfig(
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
 # logging.disable(logging.CRITICAL) # Note out to enable logging.
+
+
+MY_WEATHER = os.environ.get("MY_WEATHER")  # Get weather info site page
+
+
+def get_my_weather():
+    session = HTMLSession()
+    res = session.get(MY_WEATHER, timeout=10)
+    res.raise_for_status()
+    site_status = res.status_code
+
+    if site_status == 200:
+        css_selector = ".myforecast-current"
+        forecast_today = res.html.find(css_selector, first=True).text
+        return forecast_today
+
+
+current_forecast = get_my_weather()
+logging.info(current_forecast)
